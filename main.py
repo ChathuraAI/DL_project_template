478280635fa487c9d101804b282d173588ed5ea6
ChathuraAI <chathura.galkandage@gmail.com>
2024-08-18 19:34:06 +0100

    Evaluate model added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_basemodel import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_train_model import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_evaluate_model import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -28,7 +29,7 @@ if __name__ == '__main__':
         logger.exception(e)
         raise e
     
-    STAGE_NAME = "Train model stage"
+STAGE_NAME = "Train model stage"
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -38,3 +39,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
+
